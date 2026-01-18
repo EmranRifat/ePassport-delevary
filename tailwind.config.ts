4208f80cbc31411d7f9cbaf5d497df0531f7b9b2
rakib3012 <rakibuzzaman1177@gmail.com>
2026-01-18 16:31:33 +0600

    herou ui version update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,47 @@
-import { heroui } from '@heroui/react'
-import type { Config } from 'tailwindcss'
+import { heroui } from "@heroui/react";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
- 
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/react/**/*.{js,ts,jsx,tsx}", // ✅ required
+    "./node_modules/@heroui/react/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: { colors: { /* ... */ } },
+    extend: {
+      colors: {
+        danger: {
+        DEFAULT: '#EF4444',
+        50: '#FEF2F2',
+        100: '#FEE2E2',
+        200: '#FECACA',
+        300: '#FCA5A5',
+        400: '#F87171',
+        500: '#EF4444',
+        600: '#DC2626',
+        700: '#B91C1C',
+        800: '#991B1B',
+        900: '#7F1D1D',
+      },
+        primary: {
+          DEFAULT: "#673AB7",
+          50: "#EDE7F6",
+          100: "#D1C4E9",
+          200: "#B39DDB",
+          300: "#9575CD",
+          400: "#7E57C2",
+          500: "#673AB7",
+          600: "#5E35B1",
+          700: "#512DA8",
+          800: "#4527A0",
+          900: "#311B92",
+        },
+      },
+    },
   },
   plugins: [heroui()],
-   darkMode: "class",
-}
+  darkMode: "class",
+};
 
-export default config
-
-
- 
+export default config;
