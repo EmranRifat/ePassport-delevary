9aa77741fe265303f1c3dfc30bf80c572736cf8b
rakib3012 <rakibuzzaman1177@gmail.com>
2026-04-11 15:40:06 +0600

    fix date format in report pdf

--- src/components/Booking/BookingComponent.tsx
+++ src/components/Booking/BookingComponent.tsx
@@ -380,7 +380,7 @@ const BookingComponent = () => {
 
         {/* RPO Grid */}
         {viewMode === "grid" && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4   2xl:grid-cols-6 gap-3 sm:gap-4 md:gap-4 lg:gap-4">
             {filteredAddresses.map((address) => (
               <button
                 key={address.code}
